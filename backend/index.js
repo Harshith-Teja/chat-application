405e3f0a21608251b69ae7ced32b93a9547fbf3f
Harshith Teja <harshithtejadontoju@gmail.com>
2025-05-03 16:53:12 +0530

    added server logic

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const mongodbURL = process.env.MONGODB_URL;
+
+app.use(
+  cors({
+    origin: [process.env.ORIGIN],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+const server = app.listen(port, () => {
+  console.log("connected to the server at port:", port);
+});
+
+mongoose
+  .connect(mongodbURL)
+  .then(() => console.log("Connected to mongodb successfully"))
+  .catch((err) => console.log(err.message));
