be1a496273beaceb3387fd2d17dbc66839e6d169
Harshith Teja <harshithtejadontoju@gmail.com>
2025-05-21 23:40:39 +0530

    added protection for routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,14 +8,50 @@ import {
 import Auth from "./pages/auth/auth.jsx";
 import Chat from "./pages/chat/chat.jsx";
 import Profile from "./pages/profile/profile.jsx";
+import { useAppStore } from "./store/store";
 
 function App() {
+  const PrivateRoute = ({ children }) => {
+    const { userInfo } = useAppStore();
+    const isAuthenticated = !!userInfo;
+
+    return isAuthenticated ? children : <Navigate to="/auth" />;
+  };
+
+  const AuthRoute = ({ children }) => {
+    const { userInfo } = useAppStore();
+    const isAuthenticated = !!userInfo;
+
+    return isAuthenticated ? <Navigate to="/chat" /> : children;
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/auth"
+          element={
+            <AuthRoute>
+              <Auth />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path="/chat"
+          element={
+            <PrivateRoute>
+              <Chat />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
         <Route path="*" element={<Navigate to="/auth" />} />
       </Routes>
     </Router>
