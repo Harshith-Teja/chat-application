501bb7587f41d0e81565155ab6d18195608c443a
Harshith Teja <harshithtejadontoju@gmail.com>
2025-05-22 09:14:57 +0530

    added getUserInfo logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,42 @@ import Auth from "./pages/auth/auth.jsx";
 import Chat from "./pages/chat/chat.jsx";
 import Profile from "./pages/profile/profile.jsx";
 import { useAppStore } from "./store/store";
+import { useEffect, useState } from "react";
+import apiClient from "./lib/api-client";
+import { GET_USER_INFO } from "./utils/constants";
 
 function App() {
+  const { userInfo, setUserInfo } = useAppStore();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const response = await apiClient.get(GET_USER_INFO, {
+          withCredentials: true,
+        });
+
+        if (response.status === 200 && response.data.user.id) {
+          setUserInfo(response.data.user);
+        } else {
+          setUserInfo(undefined);
+        }
+
+        console.log({ response });
+      } catch (err) {
+        setUserInfo(undefined);
+        console.log(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (!userInfo) getUserData();
+    else setLoading(false);
+  }, [userInfo, setUserInfo]);
+
+  if (loading) return <div>Loading...</div>;
+
   const PrivateRoute = ({ children }) => {
     const { userInfo } = useAppStore();
     const isAuthenticated = !!userInfo;
