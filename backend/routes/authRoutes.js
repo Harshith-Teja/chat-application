321e12d1557c183674aca9a955a76746da594fec
Harshith Teja <harshithtejadontoju@gmail.com>
2025-06-03 09:02:08 +0530

    added routes for add profile image and remove profile image

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,17 +1,28 @@
 import { Router } from "express";
 import {
+  addProfileImage,
   getUserInfo,
   login,
+  removeProfileImage,
   signup,
   updateProfile,
 } from "../controller/authControllers.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
+import multer from "multer";
 
 const authRoutes = Router();
+const upload = multer({ dest: "uploads/profiles/" });
 
 authRoutes.post("/signup", signup);
 authRoutes.post("/login", login);
 authRoutes.get("/user-info", verifyToken, getUserInfo);
 authRoutes.post("/update-profile", verifyToken, updateProfile);
+authRoutes.post(
+  "/add-profile-image",
+  verifyToken,
+  upload.single("profile-image"),
+  addProfileImage
+);
+authRoutes.delete("/remove-profile-image", verifyToken, removeProfileImage);
 
 export default authRoutes;
