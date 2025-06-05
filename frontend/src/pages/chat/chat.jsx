7059c1ddb1c3cb05ee043333a3ad77e6b96a9213
Harshith Teja <harshithtejadontoju@gmail.com>
2025-06-05 09:24:10 +0530

    set up chat page layout

--- frontend/src/pages/chat/chat.jsx
+++ frontend/src/pages/chat/chat.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { useAppStore } from "@/store/store";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import ContactsContainer from "./components/contacts-container/contactsContainer";
+import EmptyChatContainer from "./components/empty-chat-container/emptyChatContainer";
+import ChatContainer from "./components/chat-container/chatContainer";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const { userInfo } = useAppStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userInfo.profileSetup) {
+      toast("Please setup your profile to continue");
+      navigate("/profile");
+    }
+  }, [userInfo, navigate]);
+
+  return (
+    <div className="flex h-[100vh] text-white overflow-hidden">
+      <ContactsContainer />
+      <EmptyChatContainer />
+      <ChatContainer />
+    </div>
+  );
 };
 
 export default Chat;
