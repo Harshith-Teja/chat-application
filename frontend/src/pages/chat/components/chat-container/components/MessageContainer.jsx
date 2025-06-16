ee7a28b24fc7cd8feed382c52437ada6fe2863d2
Harshith Teja <harshithtejadontoju@gmail.com>
2025-06-16 19:12:53 +0530

    added msg container

---
+++ frontend/src/pages/chat/components/chat-container/components/MessageContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MessageContainer = () => {
+  return (
+    <section className="flex-1 overflow-y-auto scrollbar-hidden p-4 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full"></section>
+  );
+};
+
+export default MessageContainer;
