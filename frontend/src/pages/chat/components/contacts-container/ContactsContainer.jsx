4541164d19dfe3c6d6657f0f7e5d9e26b0a3c3af
Harshith Teja <harshithtejadontoju@gmail.com>
2025-06-16 19:13:12 +0530

    added contacts container comp

---
+++ frontend/src/pages/chat/components/contacts-container/ContactsContainer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const ContactsContainer = () => {
+  return (
+    <article className="relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full">
+      <h1 className="pt-3">Chat App</h1>
+      <section className="my-5">
+        <div className="flex items-center justify-between pr-10">
+          <Title text="Direct Messages" />
+        </div>
+      </section>
+      <section className="my-5">
+        <div className="flex items-center justify-between pr-10">
+          <Title text="Channels" />
+        </div>
+      </section>
+    </article>
+  );
+};
+
+export default ContactsContainer;
+
+const Title = ({ text }) => {
+  return (
+    <h6 className="uppercase tracking-widest text-neutral-400 pl-10 font-light text-opacity-90 text-sm">
+      {text}
+    </h6>
+  );
+};
