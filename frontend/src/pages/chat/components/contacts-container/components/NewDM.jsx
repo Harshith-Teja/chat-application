b440ca571b513c5ac1b7f73893d48a2e9935e4ce
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-11 23:44:07 +0530

    built newDM to search and open up a contact's chat

---
+++ frontend/src/pages/chat/components/contacts-container/components/NewDM.jsx
@@ -0,0 +1,150 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import React, { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import Lottie from "react-lottie";
+import { animationDefaultOptions } from "@/lib/utils";
+import apiClient from "@/lib/api-client";
+import { SEARCH_CONTACT_ROUTES } from "@/utils/constants";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { useAppStore } from "@/store/store";
+
+const NewDM = () => {
+  const { setSelectedChatType, setSelectedChatData } = useAppStore();
+  const [openNewContactModal, setOpenNewContactModal] = useState(false);
+  const [searchedContacts, setSearchedContacts] = useState([]);
+
+  const searchContacts = async (searchTerm) => {
+    try {
+      if (searchTerm.length > 0) {
+        const response = await apiClient.post(
+          SEARCH_CONTACT_ROUTES,
+          { searchTerm },
+          { withCredentials: true }
+        );
+
+        if (response.status === 200 && response.data.contacts) {
+          setSearchedContacts(response.data.contacts);
+        }
+      } else {
+        setSearchedContacts([]);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const selectNewContact = (contact) => {
+    setOpenNewContactModal(false);
+    setSelectedChatType("contact");
+    setSelectedChatData(contact);
+    setSearchedContacts([]);
+  };
+
+  return (
+    <>
+      <Tooltip>
+        <TooltipTrigger>
+          <FaPlus
+            className="text-neutral-400 text-opacity-90 text-start hover:text-neutral-100 cursor-pointer transition-all duration-300 "
+            onClick={() => setOpenNewContactModal(true)}
+          />
+        </TooltipTrigger>
+        <TooltipContent className="bg-[#1c1b1e] border-none text-white">
+          Edit Profile
+        </TooltipContent>
+      </Tooltip>
+      <Dialog open={openNewContactModal} onOpenChange={setOpenNewContactModal}>
+        <DialogContent className="md:bg-[#1c1d25]">
+          <DialogHeader>
+            <DialogTitle className="text-white">
+              Please select a contact
+            </DialogTitle>
+            <DialogDescription></DialogDescription>
+          </DialogHeader>
+          <div>
+            <Input
+              placeholder="Search Contacts"
+              className="rounded-lg p-6 bg-[#2c2e3b] text-white border-none"
+              onChange={(e) => searchContacts(e.target.value)}
+            />
+          </div>
+          {searchedContacts.length > 0 && (
+            <ScrollArea className="h-250px ">
+              <div className="flex flex-col gap-5">
+                {searchedContacts.map((contact) => (
+                  <div
+                    key={contact._id}
+                    className="flex gap-3 items-center cursor-pointer"
+                    onClick={() => selectNewContact(contact)}
+                  >
+                    <div className="flex gap-3 items-center justify-center">
+                      <div className="w-12 h-12 relative">
+                        <Avatar className="h-12 w-12 rounded-full overflow-hidden">
+                          {contact.image ? (
+                            <AvatarImage
+                              src={`${HOST}/${contact.image}`}
+                              alt="profile"
+                              className="object-cover w-full h-full bg-black text-white"
+                            />
+                          ) : (
+                            <div
+                              className={`uppercase h-12 w-12 text-lg border-[1px] flex items-center justify-center text-white rounded-full ${contact.color}`}
+                            >
+                              {contact.firstName
+                                ? contact.firstName.charAt(0)
+                                : contact.email.charAt(0)}
+                            </div>
+                          )}
+                        </Avatar>
+                      </div>
+                      <div className="flex flex-col text-white">
+                        <span>
+                          {contact.firstName && contact.lastName
+                            ? `${contact.firstName} ${contact.lastName}`
+                            : ""}
+                        </span>
+                        <span className="text-xs">{contact.email}</span>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </ScrollArea>
+          )}
+          {searchedContacts.length <= 0 && (
+            <div className="flex-1 md:bg-[#1c1d25] md:flex mt-5 md:mt-0 flex-col justify-center items-center hidden duration-1000 transition-all">
+              <Lottie
+                isClickToPauseDisabled={true}
+                height={100}
+                width={100}
+                options={animationDefaultOptions}
+              />
+              <div className="text-opacity-80 text-white flex flex-col gap-5 items-center mt-5 lg:text-2xl text-xl transition-all duration-300 text-center">
+                <h3 className="poppins-medium">
+                  Hi<span className="text-purple-500">!</span> Search new
+                  <span className="text-purple-500"> Contact</span>
+                </h3>
+              </div>
+            </div>
+          )}
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default NewDM;
