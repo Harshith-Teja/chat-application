1891e4d8a9ef3ad7e8d535bcd9ac9b4554eab86c
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-13 21:50:06 +0530

    built model to store messages

---
+++ backend/model/messagesModel.js
@@ -0,0 +1,39 @@
+import { model, Schema } from "mongoose";
+
+const messagesSchema = new Schema({
+  sender: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  receiver: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: false,
+  },
+  messageType: {
+    type: String,
+    enum: ["text", "file"],
+    require: true,
+  },
+  content: {
+    type: String,
+    required: function () {
+      return this.messageType === "text";
+    },
+  },
+  fileUrl: {
+    type: String,
+    required: function () {
+      return this.messageType === "file";
+    },
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Message = model("Message", messagesSchema);
+
+export default Message;
