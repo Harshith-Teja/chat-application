390b781a4d1e71b055e02db7ba20ab98be9d1f9a
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-13 21:48:50 +0530

    provided socket to entire app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "./components/ui/sonner";
+import { SocketProvider } from "./components/context/SocketContext";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <>
-    <App />
-    <Toaster closeButton />
+    <SocketProvider>
+      <App />
+      <Toaster closeButton />
+    </SocketProvider>
   </>
   // </StrictMode>
 );
