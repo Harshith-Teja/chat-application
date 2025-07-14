ea3b44bcc48059da5f6d53454c27a2e29cce6806
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-14 16:12:25 +0530

    added controller logic to fetch messages of a chat

---
+++ backend/controller/messagesController.js
@@ -0,0 +1,23 @@
+import Message from "../model/messagesModel.js";
+
+export const getMessages = async (req, res, next) => {
+  try {
+    const user1 = req.userId;
+    const user2 = req.body.id;
+
+    if (!user1 || !user2)
+      return res.status(400).send("Both user Id's are required");
+
+    const messages = await Message.find({
+      $or: [
+        { sender: user1, receiver: user2 },
+        { sender: user2, receiver: user1 },
+      ],
+    }).sort({ timestamp: 1 });
+
+    return res.status(200).json({ messages });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Internal Server Error");
+  }
+};
