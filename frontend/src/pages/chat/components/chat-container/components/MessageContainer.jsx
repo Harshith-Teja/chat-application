f4784ff81cc094b1e044751c3fd84bc05e069408
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-14 14:31:20 +0530

    added logic to display the sent and received msgs in the chat

--- frontend/src/pages/chat/components/chat-container/components/MessageContainer.jsx
+++ frontend/src/pages/chat/components/chat-container/components/MessageContainer.jsx
@@ -1,8 +1,69 @@
-import React from "react";
+import { useAppStore } from "@/store/store";
+import moment from "moment";
+import React, { useEffect, useRef } from "react";
 
 const MessageContainer = () => {
+  const scrollRef = useRef();
+  const { selectedChatType, selectedChatData, userInfo, selectedChatMessages } =
+    useAppStore();
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [selectedChatMessages]);
+
+  const renderDmMessages = (message) => {
+    return (
+      <div
+        className={`${
+          message.sender === selectedChatData._id ? "text-left" : "text-right"
+        }`}
+      >
+        {message.messageType === "text" && (
+          <div
+            className={`${
+              message.sender !== selectedChatData._id
+                ? "bg-[#8417ff]/5 text-[#8417ff] border-[#8417ff]/50"
+                : "bg-[#2a2b33]/5 text-white/80 border-[#ffffff]/20"
+            } border inline-block p-4 rounded my-1 max-w-[50%] break-words`}
+          >
+            {message.content}
+          </div>
+        )}
+        <div className="text-xs text-gray-600">
+          {moment(message.timestamp).format("LT")}
+        </div>
+      </div>
+    );
+  };
+
+  const renderMessages = () => {
+    let lastDate = null;
+
+    return selectedChatMessages.map((message) => {
+      const messageDate = moment(message.timestamp).format("YYYY-MM-DD");
+      const showDate = messageDate !== lastDate;
+      lastDate = messageDate;
+
+      return (
+        <section key={message._id}>
+          {showDate && (
+            <div className="text-center text-gray-500 my-2">
+              {moment(message.timestamp).format("LL")}
+            </div>
+          )}
+          {selectedChatType === "contact" && renderDmMessages(message)}
+        </section>
+      );
+    });
+  };
+
   return (
-    <section className="flex-1 overflow-y-auto scrollbar-hidden p-4 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full"></section>
+    <section className="flex-1 overflow-y-auto scrollbar-hidden p-4 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full">
+      {renderMessages()}
+      <div ref={scrollRef}></div>
+    </section>
   );
 };
 
