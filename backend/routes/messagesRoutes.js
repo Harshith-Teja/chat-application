5f526731a00d4a3ceeb788a8532a17edc019e9d1
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-15 20:46:33 +0530

    added route to send files

--- backend/routes/messagesRoutes.js
+++ backend/routes/messagesRoutes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { getMessages } from "../controller/messagesController.js";
+import { getMessages, uploadFile } from "../controller/messagesController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
+import multer from "multer";
 
 const messageRoutes = Router();
+const upload = multer({ dest: "/uploads/files" });
 
 messageRoutes.post("/get-messages", verifyToken, getMessages);
+messageRoutes.post(
+  "/upload-file",
+  verifyToken,
+  upload.single("file"),
+  uploadFile
+);
 
 export default messageRoutes;
