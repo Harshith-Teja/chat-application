c10b52dccd74b6bdbfe2786634936c755a5437c3
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-15 17:44:18 +0530

    added route to get contacts for DM list

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { verifyToken } from "../middleware/authMiddleware.js";
-import { searchContacts } from "../controller/ContactsController.js";
+import {
+  getContactsForDmList,
+  searchContacts,
+} from "../controller/ContactsController.js";
 
 const contactsRoutes = Router();
 
 contactsRoutes.post("/search", verifyToken, searchContacts);
+contactsRoutes.get("/get-contacts-for-dm", verifyToken, getContactsForDmList);
 
 export default contactsRoutes;
