7f23f19b2025d9661ac25f6c7c30b693ee58d791
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-16 14:51:40 +0530

    added logic to show upload and download progress

--- frontend/src/pages/chat/chat.jsx
+++ frontend/src/pages/chat/chat.jsx
@@ -7,7 +7,14 @@ import EmptyChatContainer from "./components/empty-chat-container/emptyChatConta
 import ChatContainer from "./components/chat-container/chatContainer";
 
 const Chat = () => {
-  const { userInfo, selectedChatType } = useAppStore();
+  const {
+    userInfo,
+    selectedChatType,
+    isUploading,
+    isDownloading,
+    fileUploadProgress,
+    fileDownloadProgress,
+  } = useAppStore();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -19,6 +26,18 @@ const Chat = () => {
 
   return (
     <div className="flex h-[100vh] text-white overflow-hidden">
+      {isUploading && (
+        <div className="h-[100vh] w-[100vw] fixed top-0 z-10 left-0 bg-black/80 flex items-center justify-center flex-col gap-5 backdrop-blur-lg">
+          <h5 className="text-5xl animate-pulse">Uploading file...</h5>
+          {fileUploadProgress}%
+        </div>
+      )}
+      {isDownloading && (
+        <div className="h-[100vh] w-[100vw] fixed top-0 z-10 left-0 bg-black/80 flex items-center justify-center flex-col gap-5 backdrop-blur-lg">
+          <h5 className="text-5xl animate-pulse">Downloading file...</h5>
+          {fileDownloadProgress}%
+        </div>
+      )}
       <ContactsContainer />
       {selectedChatType === undefined ? (
         <EmptyChatContainer />
