c0b6942161b26491ac43838f60548bfec62790e0
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-16 15:33:07 +0530

    added route to getAllContacts

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { verifyToken } from "../middleware/authMiddleware.js";
 import {
+  getAllContacts,
   getContactsForDmList,
   searchContacts,
 } from "../controller/ContactsController.js";
@@ -9,5 +10,6 @@ const contactsRoutes = Router();
 
 contactsRoutes.post("/search", verifyToken, searchContacts);
 contactsRoutes.get("/get-contacts-for-dm", verifyToken, getContactsForDmList);
+contactsRoutes.get("/get-all-contacts", verifyToken, getAllContacts);
 
 export default contactsRoutes;
