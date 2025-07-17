99b175131507b8ba4fddb837629f4c2f103b44e4
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-17 16:14:41 +0530

    fixed the typo in references

--- backend/model/channelModel.js
+++ backend/model/channelModel.js
@@ -2,9 +2,9 @@ import { model, Schema } from "mongoose";
 
 const channelSchema = new Schema({
   name: { type: String, required: true },
-  members: [{ type: Schema.ObjectId, ref: "Users", required: true }],
-  admin: { type: Schema.ObjectId, ref: "Users", required: true },
-  messages: [{ type: Schema.ObjectId, ref: "Messages", required: false }],
+  members: [{ type: Schema.ObjectId, ref: "User", required: true }],
+  admin: { type: Schema.ObjectId, ref: "User", required: true },
+  messages: [{ type: Schema.ObjectId, ref: "Message", required: false }],
   createdAt: { type: Date, default: Date.now() },
   updatedAt: { type: Date, default: Date.now() },
 });
