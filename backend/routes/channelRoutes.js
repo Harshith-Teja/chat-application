436067e3d206fb65faa600dbd4f51fb22bcfe225
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-17 12:18:44 +0530

    added route to get all user channels

--- backend/routes/channelRoutes.js
+++ backend/routes/channelRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { createChannel } from "../controller/channelController.js";
+import {
+  createChannel,
+  getUserChannels,
+} from "../controller/channelController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 
 const channelRoutes = Router();
 
 channelRoutes.post("/create-channel", verifyToken, createChannel);
+channelRoutes.get("/get-user-channels", verifyToken, getUserChannels);
 
 export default channelRoutes;
