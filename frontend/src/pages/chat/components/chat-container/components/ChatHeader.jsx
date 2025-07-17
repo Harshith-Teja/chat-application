65dcabd4c5bfdaa6679c565631c51325f8619963
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-17 12:28:56 +0530

    added logic to display the chat header of the channel when you click on a channel

--- frontend/src/pages/chat/components/chat-container/components/ChatHeader.jsx
+++ frontend/src/pages/chat/components/chat-container/components/ChatHeader.jsx
@@ -13,27 +13,34 @@ const ChatHeader = () => {
       <div className="flex gap-5 items-center">
         <div className="flex gap-3 items-center justify-center">
           <div className="w-12 h-12 relative">
-            <Avatar className="h-12 w-12 rounded-full overflow-hidden">
-              {selectedChatData.image ? (
-                <AvatarImage
-                  src={`${HOST}/${selectedChatData.image}`}
-                  alt="profile"
-                  className="object-cover w-full h-full bg-black text-white"
-                />
-              ) : (
-                <div
-                  className={`uppercase h-12 w-12 text-lg border-[1px] flex items-center justify-center text-white rounded-full ${getColor(
-                    selectedChatData.color
-                  )}`}
-                >
-                  {selectedChatData.firstName
-                    ? selectedChatData.firstName.charAt(0)
-                    : selectedChatData.email.charAt(0)}
-                </div>
-              )}
-            </Avatar>
+            {selectedChatType === "contact" ? (
+              <Avatar className="h-12 w-12 rounded-full overflow-hidden">
+                {selectedChatData.image ? (
+                  <AvatarImage
+                    src={`${HOST}/${selectedChatData.image}`}
+                    alt="profile"
+                    className="object-cover w-full h-full bg-black text-white"
+                  />
+                ) : (
+                  <div
+                    className={`uppercase h-12 w-12 text-lg border-[1px] flex items-center justify-center text-white rounded-full ${getColor(
+                      selectedChatData.color
+                    )}`}
+                  >
+                    {selectedChatData.firstName
+                      ? selectedChatData.firstName.charAt(0)
+                      : selectedChatData.email.charAt(0)}
+                  </div>
+                )}
+              </Avatar>
+            ) : (
+              <div className="bg-[#ffffff22] h-10 w-10 flex items-center justify-center rounded-full">
+                #
+              </div>
+            )}
           </div>
           <div>
+            {selectedChatType === "channel" && selectedChatData.name}
             {selectedChatType === "contact" &&
               (selectedChatData.firstName
                 ? `${selectedChatData.firstName} ${selectedChatData.lastName}`
