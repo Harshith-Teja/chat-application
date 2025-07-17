1c9717bfef4883109f4ee30edcc8a0590ab813e8
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-17 16:16:54 +0530

    added logic to send a channel message from client side to server

--- frontend/src/pages/chat/components/chat-container/components/MessageBar.jsx
+++ frontend/src/pages/chat/components/chat-container/components/MessageBar.jsx
@@ -48,7 +48,17 @@ const MessageBar = () => {
         messageType: "text",
         fileUrl: undefined,
       });
+    } else if (selectedChatType === "channel") {
+      socket.emit("send-channel-message", {
+        sender: userInfo.id,
+        content: message,
+        messageType: "text",
+        fileUrl: undefined,
+        channelId: selectedChatData._id,
+      });
     }
+
+    setMessage("");
   };
 
   const handleAttachmentClick = () => {
@@ -81,6 +91,14 @@ const MessageBar = () => {
               receiver: selectedChatData._id,
               messageType: "file",
               fileUrl: response.data.filePath,
+            });
+          } else if (selectedChatType === "channel") {
+            socket.emit("send-channel-message", {
+              sender: userInfo.id,
+              content: undefined,
+              messageType: "file",
+              fileUrl: response.data.filePath,
+              channelId: selectedChatData._id,
             });
           }
         }
