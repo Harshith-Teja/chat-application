387d23751f9f15c9895dd9fc142dfc2e8a3262d3
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-17 16:15:26 +0530

    added logic to receive a channel message on the client side

--- frontend/src/components/context/SocketContext.jsx
+++ frontend/src/components/context/SocketContext.jsx
@@ -38,7 +38,20 @@ export const SocketProvider = ({ children }) => {
         }
       };
 
+      const handleRecieveChannelMessage = (message) => {
+        const { selectedChatData, selectedChatType, addMessage } =
+          useAppStore.getState();
+
+        if (
+          selectedChatType !== undefined &&
+          selectedChatData._id === message.channelId
+        ) {
+          addMessage(message);
+        }
+      };
+
       socket.current.on("receiveMessage", handleReceiveMessage);
+      socket.current.on("recieve-channel-message", handleRecieveChannelMessage);
 
       return () => {
         socket.current.disconnect();
