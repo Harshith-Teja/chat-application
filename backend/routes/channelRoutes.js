3e0257dd202958d44496293d8c660706a024d296
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-17 18:56:08 +0530

    added route to fetch all chat messages for a channel

--- backend/routes/channelRoutes.js
+++ backend/routes/channelRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   createChannel,
+  getChannelMessages,
   getUserChannels,
 } from "../controller/channelController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
@@ -9,5 +10,10 @@ const channelRoutes = Router();
 
 channelRoutes.post("/create-channel", verifyToken, createChannel);
 channelRoutes.get("/get-user-channels", verifyToken, getUserChannels);
+channelRoutes.get(
+  "/get-channel-messages/:channelId",
+  verifyToken,
+  getChannelMessages
+);
 
 export default channelRoutes;
