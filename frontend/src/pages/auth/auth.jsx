19be9d9bb78b8d80adedf4b10a825cd8fec9ba80
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-18 12:40:59 +0530

    fixed the alignment issue with the welcome message and the login image

--- frontend/src/pages/auth/auth.jsx
+++ frontend/src/pages/auth/auth.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import victory from "@/assets/victory.svg";
-import background from "@/assets/login2.png";
+import { IoIosChatbubbles } from "react-icons/io";
+import background from "@/assets/image.png";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -91,14 +92,18 @@ const Auth = () => {
 
   return (
     <main className="h-[100vh] w-[100vw] flex items-center justify-center">
-      <article className="h-[80vh] w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] bg-white border-2 border-white text-opacity-90 shadow-2xl rounded-3xl grid xl:grid-cols-2">
+      <article className="h-[80vh] w-[80vw] lg:w-[70vw] xl:w-[60vw] bg-white border-2 border-white text-opacity-90 shadow-2xl rounded-3xl grid xl:grid-cols-2">
         <section className="flex flex-col gap-10 items-center justify-center">
           <div className="flex items-center justify-center flex-col">
-            <div className="flex items-center justify-center">
-              <h1 className="text-5xl font-bold md:text-6xl">Welcome</h1>
-              <img src={victory} alt="victory emoji" className="h-[100px]" />
+            <div className="flex items-center justify-center text-center p-4">
+              <h1 className="text-4xl font-bold">
+                Welcome to{" "}
+                <span className="inline-flex whitespace-nowrap gap-1">
+                  HolaChat <IoIosChatbubbles />
+                </span>
+              </h1>
             </div>
-            <p className="font-medium text-center">
+            <p className="font-medium text-center mt-2">
               Fill in the details to get started with the chat app
             </p>
           </div>
@@ -167,7 +172,11 @@ const Auth = () => {
           </div>
         </section>
         <section className="hidden xl:flex justify-center items-center">
-          <img src={background} alt="background login" className="h-[700px]" />
+          <img
+            src={background}
+            alt="background login"
+            className="h-[500px] w-[500px] object-cover p-4"
+          />
         </section>
       </article>
     </main>
