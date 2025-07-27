9d429e7cd9a586b5462398014866f96cecf23196
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-27 16:28:27 +0530

    refactor: optimize ChatContainer component using memo for performance

--- frontend/src/pages/chat/components/chat-container/ChatContainer.jsx
+++ frontend/src/pages/chat/components/chat-container/ChatContainer.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { memo } from "react";
 import ChatHeader from "./components/ChatHeader";
 import MessageBar from "./components/MessageBar";
 import MessageContainer from "./components/MessageContainer";
 
-const ChatContainer = () => {
+const ChatContainer = memo(() => {
   return (
     <div className="fixed top-0 h-[100vh] w-[100vw] bg-[#1c1d25] flex flex-col md:static md:flex-1">
       <ChatHeader />
@@ -11,6 +11,6 @@ const ChatContainer = () => {
       <MessageBar />
     </div>
   );
-};
+});
 
 export default ChatContainer;
