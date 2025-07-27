2296cd291156fd0d2ca53869d53255e204f2e2e3
Harshith Teja <harshithtejadontoju@gmail.com>
2025-07-27 16:34:20 +0530

    refactor: prevented unnecessary re-renders in the global state by using local state to display messages

--- frontend/src/pages/chat/components/contacts-container/components/ContactList.jsx
+++ frontend/src/pages/chat/components/contacts-container/components/ContactList.jsx
@@ -1,10 +1,15 @@
 import { getColor } from "@/lib/utils";
 import { useAppStore } from "@/store/store";
 import { HOST } from "@/utils/constants";
-import React from "react";
+import React, { memo, useEffect, useState } from "react";
 import { Avatar, AvatarImage } from "../../../../../components/ui/avatar";
+import apiClient from "@/lib/api-client";
+import { GET_DM_CONTACTS_ROUTES } from "@/utils/constants";
+import { isEqual } from "lodash";
 
-const ContactList = ({ contacts, isChannel = false }) => {
+const ContactList = memo(() => {
+  const [DmContacts, setDMContacts] = useState([]);
+
   const {
     selectedChatData,
     setSelectedChatData,
@@ -12,29 +17,42 @@ const ContactList = ({ contacts, isChannel = false }) => {
     setSelectedChatMessages,
   } = useAppStore();
 
+  useEffect(() => {
+    const getContacts = async () => {
+      const response = await apiClient.get(GET_DM_CONTACTS_ROUTES, {
+        withCredentials: true,
+      });
+
+      if (!isEqual(DmContacts, response.data.contacts)) {
+        setDMContacts(response.data.contacts);
+      }
+    };
+
+    getContacts();
+  }, []);
+
   const handleClick = (contact) => {
-    if (isChannel) setSelectedChatType("channel");
-    else setSelectedChatType("contact");
-
+    setSelectedChatType("contact");
     setSelectedChatData(contact);
-
-    if (selectedChatData && selectedChatData._id !== contact._id)
+    if (selectedChatData && selectedChatData._id !== contact._id) {
       setSelectedChatMessages([]);
+    }
   };
+
   return (
     <section className="mt-5">
-      {contacts.map((contact) => (
-        <div
-          key={contact._id}
-          className={`pl-10 py-2 transition-all duration-300 cursor-pointer ${
-            selectedChatData && selectedChatData._id === contact._id
-              ? "bg-[#8417ff] hover:bg-[#8417ff]"
-              : "hover:bg-[#f1f1f111]"
-          }`}
-          onClick={() => handleClick(contact)}
-        >
-          <div className="flex gap-5 items-center justify-start text-neutral-300">
-            {!isChannel && (
+      {DmContacts.length > 0 &&
+        DmContacts.map((contact) => (
+          <div
+            key={contact._id}
+            className={`pl-10 py-2 transition-all duration-300 cursor-pointer ${
+              selectedChatData && selectedChatData._id === contact._id
+                ? "bg-[#8417ff] hover:bg-[#8417ff]"
+                : "hover:bg-[#f1f1f111]"
+            }`}
+            onClick={() => handleClick(contact)}
+          >
+            <div className="flex gap-5 items-center justify-start text-neutral-300">
               <Avatar className="h-10 w-10 rounded-full overflow-hidden">
                 {contact.image ? (
                   <AvatarImage
@@ -53,31 +71,17 @@ const ContactList = ({ contacts, isChannel = false }) => {
                     } uppercase h-10 w-10 text-lg border-[1px] flex items-center justify-center text-white rounded-full`}
                   >
                     {contact.firstName
-                      ? contact.firstName.charAt(0)
-                      : contact.email.charAt(0)}
+                      ? contact?.firstName?.charAt(0)
+                      : contact?.email?.charAt(0)}
                   </div>
                 )}
               </Avatar>
-            )}
-            {isChannel && (
-              <div className="bg-[#ffffff22] h-10 w-10 flex items-center justify-center rounded-full">
-                #
-              </div>
-            )}
-            {isChannel ? (
-              <span>{contact.name}</span>
-            ) : (
-              <span>
-                {contact.firstName
-                  ? `${contact.firstName} ${contact.lastName}`
-                  : contact.email}
-              </span>
-            )}
+              <span>{`${contact.firstName} ${contact.lastName}`}</span>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </section>
   );
-};
+});
 
 export default ContactList;
