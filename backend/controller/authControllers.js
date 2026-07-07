15bf23fefe18eef597d2ef1bbdfa7c5bcf362336
Harshith Teja <harshithtejadontoju@gmail.com>
2026-07-07 23:32:07 +0530

    feat: add error handling to login process and improve response messages

--- backend/controller/authControllers.js
+++ backend/controller/authControllers.js
@@ -38,37 +38,42 @@ export const signup = async (req, res, next) => {
 };
 
 export const login = async (req, res, next) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  if (!email || !password)
-    return res.status(400).send("Email and password are required");
+    if (!email || !password)
+      return res.status(400).send("Email and password are required");
 
-  const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-  if (!user) return res.status(404).send("Email not found");
+    if (!user) return res.status(404).send("Email not found");
 
-  const pwdMatch = await bcrypt.compare(password, user.password);
+    const pwdMatch = await bcrypt.compare(password, user.password);
 
-  if (!pwdMatch) return res.status(400).send("Password is incorrect");
+    if (!pwdMatch) return res.status(400).send("Password is incorrect");
 
-  const accessToken = jwt.sign(
-    { email, userId: user.id },
-    process.env.JWT_KEY,
-    { expiresIn: maxAge }
-  );
+    const accessToken = jwt.sign(
+      { email, userId: user.id },
+      process.env.JWT_KEY,
+      { expiresIn: maxAge }
+    );
 
-  res.cookie("jwt", accessToken, { maxAge, secure: true, sameSite: "None" });
+    res.cookie("jwt", accessToken, { maxAge, secure: true, sameSite: "None" });
 
-  return res.status(200).json({
-    user: {
-      id: user.id,
-      email: user.email,
-      profileSetup: user.profileSetup,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      color: user.color,
-    },
-  });
+    return res.status(200).json({
+      user: {
+        id: user.id,
+        email: user.email,
+        profileSetup: user.profileSetup,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        color: user.color,
+      },
+    });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send("Internal Server Error");
+  }
 };
 
 export const getUserInfo = async (req, res, next) => {
