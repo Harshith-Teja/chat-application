d42908e80fda980ddb837ea49057f0c34d17fc9d
Harshith Teja <harshithtejadontoju@gmail.com>
2026-08-07 20:23:58 +0530

    feat: refactor contact list to use directMessagesContacts global state and improve unread msg count handling

--- frontend/src/pages/chat/components/contacts-container/components/ContactList.jsx
+++ frontend/src/pages/chat/components/contacts-container/components/ContactList.jsx
@@ -5,16 +5,16 @@ import React, { memo, useEffect, useState } from "react";
 import { Avatar, AvatarImage } from "../../../../../components/ui/avatar";
 import apiClient from "@/lib/api-client";
 import { GET_DM_CONTACTS_ROUTES } from "@/utils/constants";
-import { isEqual } from "lodash";
 
 const ContactList = memo(() => {
-  const [DmContacts, setDMContacts] = useState([]);
-
   const {
+    directMessagesContacts,
+    setDirectMessagesContacts,
     selectedChatData,
     setSelectedChatData,
     setSelectedChatType,
     setSelectedChatMessages,
+    clearUnreadCount,
   } = useAppStore();
 
   useEffect(() => {
@@ -23,17 +23,19 @@ const ContactList = memo(() => {
         withCredentials: true,
       });
 
-      if (!isEqual(DmContacts, response.data.contacts)) {
-        setDMContacts(response.data.contacts);
+      if (response.data.contacts) {
+        setDirectMessagesContacts(response.data.contacts);
       }
     };
 
     getContacts();
-  }, []);
+  }, [setDirectMessagesContacts]);
 
   const handleClick = (contact) => {
     setSelectedChatType("contact");
     setSelectedChatData(contact);
+    clearUnreadCount(contact._id, "contact"); //clear unread count when contact is selected
+
     if (selectedChatData && selectedChatData._id !== contact._id) {
       setSelectedChatMessages([]);
     }
@@ -41,8 +43,8 @@ const ContactList = memo(() => {
 
   return (
     <section className="mt-5">
-      {DmContacts.length > 0 &&
-        DmContacts.map((contact) => (
+      {directMessagesContacts.length > 0 &&
+        directMessagesContacts.map((contact) => (
           <div
             key={contact._id}
             className={`pl-10 py-2 transition-all duration-300 cursor-pointer ${
@@ -52,31 +54,40 @@ const ContactList = memo(() => {
             }`}
             onClick={() => handleClick(contact)}
           >
-            <div className="flex gap-5 items-center justify-start text-neutral-300">
-              <Avatar className="h-10 w-10 rounded-full overflow-hidden">
-                {contact.image ? (
-                  <AvatarImage
-                    src={`${HOST}/${contact.image}`}
-                    alt="profile"
-                    className="bg-cover w-full h-full rounded-full"
-                  />
-                ) : (
-                  <div
-                    className={` ${
-                      selectedChatData &&
-                      selectedChatData._id &&
-                      selectedChatData._id === contact._id
-                        ? "bg-[ffffff22] border border-white/70"
-                        : getColor(contact.color)
-                    } uppercase h-10 w-10 text-lg border-[1px] flex items-center justify-center text-white rounded-full`}
-                  >
-                    {contact.firstName
-                      ? contact?.firstName?.charAt(0)
-                      : contact?.email?.charAt(0)}
-                  </div>
-                )}
-              </Avatar>
-              <span>{`${contact.firstName} ${contact.lastName}`}</span>
+            <div className="flex gap-5 items-center justify-between text-neutral-300 w-full pr-10">
+              <div className="flex gap-5 items-center">
+                <Avatar className="h-10 w-10 rounded-full overflow-hidden">
+                  {contact.image ? (
+                    <AvatarImage
+                      src={`${HOST}/${contact.image}`}
+                      alt="profile"
+                      className="bg-cover w-full h-full rounded-full"
+                    />
+                  ) : (
+                    <div
+                      className={` ${
+                        selectedChatData &&
+                        selectedChatData._id &&
+                        selectedChatData._id === contact._id
+                          ? "bg-[ffffff22] border border-white/70"
+                          : getColor(contact.color)
+                      } uppercase h-10 w-10 text-lg border-[1px] flex items-center justify-center text-white rounded-full`}
+                    >
+                      {contact.firstName
+                        ? contact?.firstName?.charAt(0)
+                        : contact?.email?.charAt(0)}
+                    </div>
+                  )}
+                </Avatar>
+                <span>{`${contact.firstName} ${contact.lastName}`}</span>
+              </div>
+
+              {/* Unread Badge UI */}
+              {contact.unreadCount > 0 && (
+                <span className="bg-pink-500 text-white text-xs font-bold px-2 py-1 rounded-full">
+                  {contact.unreadCount}
+                </span>
+              )}
             </div>
           </div>
         ))}
